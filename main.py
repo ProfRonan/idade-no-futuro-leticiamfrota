82b0e03eadb1ef9e29c174e13d6e0b0aa75086f1
leticiamfrota <leticiafrotamesquita@alu.ufc.br>
2023-03-26 23:12:16 -0300

    feito

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-# -*- coding: utf-8 -*-
-anoatual=int(input('Ano Atual: '))
-idadeatual=int(input('Idade Atual: '))
-outroano=int(input('Outro ano: '))
-nome=input('Nome: ')
-idade=int(idadeatual + outroano - anoatual)
-if idade > 0:
-    print(nome,',', 'no ano de',outroano, 'você terá', idade, 'anos')
+anoatual = input()
+anoatual = int(anoatual)
+
+idadeatual = input()
+idadeatual = int(idadeatual)
+
+outroano = input()
+outroano = int(outroano)
+
+nome = input()
+
+idade = idadeatual + outroano - anoatual
+
+
+print(nome,',', 'no ano de', outroano, 'você terá', idade, 'anos')
